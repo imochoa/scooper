2109079051536b4f917d59a12f33d85e037751b1
imochoa <nachomartinochoa@gmail.com>
2021-08-04 11:42:49 +0200

    - added ok file

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 # Copyright Spanflug Technologies GmbH
 
-from distutils.core import setup
 import os
+from distutils.core import setup
 
 REPO_DIR = os.path.dirname(__file__)
-with open(os.path.join(REPO_DIR, 'requirements.txt'), 'r') as f:
+with open(os.path.join(REPO_DIR, "requirements.txt")) as f:
     requirements = f.read().splitlines()
 
-scripts = [os.path.join('bin', s) for s in os.listdir(os.path.join(REPO_DIR, 'bin'))]
+scripts = [os.path.join("bin", s) for s in os.listdir(os.path.join(REPO_DIR, "bin"))]
 
-with open(os.path.join(REPO_DIR, 'README.md'), 'r') as f:
+with open(os.path.join(REPO_DIR, "README.md")) as f:
     long_description = f.read().splitlines()
 
 setup(
-    name='scooper',
-    version='1.0.0',
-    packages=['pyscooper',
-        ],
+    name="scooper",
+    version="1.0.0",
+    packages=[
+        "pyscooper",
+    ],
     scripts=scripts,
-    license='MIT',
-    install_requires=requirements
+    license="MIT",
+    install_requires=requirements,
 )
